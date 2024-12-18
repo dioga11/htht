680bb7b7270c294c1c39154d9e0548d68a62514f
NGUYENTRIEUPHUC <nguyentrieuphuc14@gmail.com>
2024-12-18 12:32:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-E=exit
+O='eJwrTwIAAVIA2g=='
+N=input
+G=exit
 C=print
 import zlib as A,base64 as B,os,requests as D
 C((lambda s:A.decompress(B.b64decode(s)).decode())('eJzjUgACXQWP0EhXP4UQD39HTwUPIA5xdAaKZpSUFBRb6etnlFam5pVk5CdmZiRmliQm6yVl5uvlZOZlc3EBACjSExg='))
@@ -6,24 +8,40 @@ C((lambda s:A.decompress(B.b64decode(s)).decode())('eJxTUFBQ0FVwd/R1VTgy4dj6h7sX
 C((lambda s:A.decompress(B.b64decode(s)).decode())('eJwBNADL/yAgICAtIEtFWSBT4busIEThu6RORyBIVVnhu4BOIFRIT+G6oEkgSOG6okkgVOG6tkMgCgoINBTk'))
 C((lambda s:A.decompress(B.b64decode(s)).decode())('eJxTUFBQ0FXwyczLVvAtTVTwTq1UUFMIyTi8JS9dISQzTyHs4e5GBffE3FQFKy4uADUvDjw='))
 C((lambda s:A.decompress(B.b64decode(s)).decode())('eJxTUACBjJKSgmIrff2M0srUvJKM/MTMjMTMksRkvaTMfL2czLxsLi4AD48NjA=='))
-C((lambda s:A.decompress(B.b64decode(s)).decode())('eJxTUFBQ0FVwerhrcZ6C88NdawsU/DKAVImC+8Ndy/PAnKUlClYKhub6hkb6RgZGJgpcXABHHROp'))
+C((lambda s:A.decompress(B.b64decode(s)).decode())('eJxTUFBQ0FWI8PRTCAw9PFfB2+Nwo7OHQliop4LP4Ul+7grOHg93LYhU8HX09NMLiFQ4MuHh7i4/DwXvwz1gdpOC88NdawIU/IDK1oQohHgcW/9w9xygvojQyMNdfgohQYcbgcp9Hu6e5snFBQDkjixL'))
 C((lambda s:A.decompress(B.b64decode(s)).decode())('eJxTUFBQ0FXwONwcqRDi4eir4O7pqODkH6EQ5ejjr3BkwsPdTQp+Hg93rfEDco6tf7h7kTNQ3eEpfu4KIZ5+Ck4Pdy3yU3AGygdAlIUouD/ctcwPzFkSAqQOdypwcQEAohoo9g=='))
-I=(lambda s:A.decompress(B.b64decode(s)).decode())('eJzLKCkpKLbS108syNRNyi/Ry8/LycxLBXH1y4z1s1Mri/VKKkoADVMN5Q==')
-try:J=D.get(I);H=J.text
-except D.RequestException as F:C(f"Không thể tải key. Lỗi: {F}");E(1)
-if not H:C((lambda s:A.decompress(B.b64decode(s)).decode())('eJzzzji8JS9doSTj4e5mhZKHuxZnKmSnVuopeGcCBXIVSooSFXIe7lqYqXBk4rEND3fPBapNebhrdZ5CRv7DXduTFbIf7tpfopD3cPfETIVcoMK8dD0AvToqCw=='));E(1)
-K=input((lambda s:A.decompress(B.b64decode(s)).decode())('eJzzy3i4a22BQnZqpcKRiQ93NyuUZD7ctb9AoeTh7qXJCsmHF2SCxHdtL7FSAADsLBZP'))
-if K not in H:C((lambda s:A.decompress(B.b64decode(s)).decode())('eJzj8k6tVMjOOLwlL13hyMTDu/LSdRTKSjMVcg5vAorkZB5elaeQ8XB3u0JiSm5mnkJuaaJCdmqlHgCYhxdO'));E(1)
-L=(lambda s:A.decompress(B.b64decode(s)).decode())('eJzLKCkpKLbS108syNRNyi/Ry8/LycxLBXH1y4z1i0rz9AoqAfH2DQc=')
-G=(lambda s:A.decompress(B.b64decode(s)).decode())('eJwrKs3TK6gEAAiWAm0=')
-if not os.path.isfile(G):
+P=(lambda s:A.decompress(B.b64decode(s)).decode())('eJzLKCkpKLbS108syNRNyi/Ry8/LycxLBXH1y4z1s1Mri/VKKkoADVMN5Q==')
+Q=(lambda s:A.decompress(B.b64decode(s)).decode())('eJzLKCkpKLbS108syNRNyi/Ry8/LycxLBXH1y4z1y1KLijPz8/RKKkoAOjEPLw==')
+J=(lambda s:A.decompress(B.b64decode(s)).decode())('eJwz1DPQMwAAAs4A7g==')
+K=(lambda s:A.decompress(B.b64decode(s)).decode())('eJzLKCkpKLbS108syNRNyi/Ry8/LycxLBXH1y4z1i0rz9AoqAfH2DQc=')
+F=(lambda s:A.decompress(B.b64decode(s)).decode())('eJwrKs3TK6gEAAiWAm0=')
+try:R=D.get(P);L=R.text
+except D.RequestException as E:C(f"Không thể tải key. Lỗi: {E}");G(1)
+if not L:C((lambda s:A.decompress(B.b64decode(s)).decode())('eJzzzji8JS9doSTj4e5mhZKHuxZnKmSnVuopeGcCBXIVSooSFXIe7lqYqXBk4rEND3fPBapNebhrdZ5CRv7DXduTFbIf7tpfopD3cPfETIVcoMK8dD0AvToqCw=='));G(1)
+S=N((lambda s:A.decompress(B.b64decode(s)).decode())('eJzzy3i4a22BQnZqpcKRiQ93NyuUZD7ctb9AoeTh7qXJCsmHF2SCxHdtL7FSAADsLBZP'))
+if S not in L:C((lambda s:A.decompress(B.b64decode(s)).decode())('eJzj8k6tVMjOOLwlL13hyMTDu/LSdRTKSjMVcg5vAorkZB5elaeQ8XB3u0JiSm5mnkJuaaJCdmqlHgCYhxdO'));G(1)
+try:
+	T=D.get(Q);M=T.text.strip();C(f"\nPhiên bản hiện tại: {J}");C(f"Phiên bản mới nhất: {M}")
+	if J!=M:
+		C((lambda s:A.decompress(B.b64decode(s)).decode())('eJzjCsjIPLwqTyHp4a7FeQq5D3fPzlQ4MvHwYoXkw5sVih/u2pqnqOD0cNfCPIXkh7uWg8m1BQp5GUCqRA8Ar7EfAg=='));U=N("Nhập (lambda s: zlib.decompress(base64.b64decode(s)).decode())('eJwzBAAAMgAy') để cập nhật tự động, hoặc bất kỳ phím nào để bỏ qua: ").strip()
+		if U==(lambda s:A.decompress(B.b64decode(s)).decode())('eJwzBAAAMgAy'):
+			if os.path.isfile(F):os.remove(F);C((lambda s:A.decompress(B.b64decode(s)).decode())('eJw7MuHwYoWKw5sTFdIyc1IVko+u1AMAYjMJCQ=='))
+			C((lambda s:A.decompress(B.b64decode(s)).decode())('eJw7MiExL12h5OGuxZkKBRmZh1flKSQBOXkKuQ93z87U09MDABhyD+c='))
+			try:
+				H=D.get(K)
+				with open(F,(lambda s:A.decompress(B.b64decode(s)).decode())(O))as I:I.write(H.content)
+				C((lambda s:A.decompress(B.b64decode(s)).decode())('eJxzfrhrbYFCXgaQKlEoyTi8IC9DIfnwlrx0RQVnoODCSoUcIJmpkJxxbMOxhXnpCiVFh9cA1RyZ+HB3s0Lxw91rFVIe7l4KlCjIyDy8Kk8h6eGuxXkKuQ93z87UAwCgLS87'));G(0)
+			except D.RequestException as E:C(f"Tải file thất bại! Lỗi: {E}");G(1)
+		else:C((lambda s:A.decompress(B.b64decode(s)).decode())('eJxzerhrYZ7CkYmHFyskPdzdr1BYmqiQ/HDX2gKFvAwgVaKnEJCReXhVHlB21+I8heSjKxWKH+7aC9RxbMPD3YuTgbzdaxVSHu5empeuBwANgihO'))
+except D.RequestException as E:C(f"Không thể kiểm tra phiên bản mới. Lỗi: {E}")
+if not os.path.isfile(F):
 	C((lambda s:A.decompress(B.b64decode(s)).decode())('eJw7MiExL12h5OGuxZkKaZk5qQp6enoAY2AIMA=='))
 	try:
-		M=D.get(L)
-		with open(G,(lambda s:A.decompress(B.b64decode(s)).decode())('eJwrTwIAAVIA2g=='))as N:N.write(M.content)
+		H=D.get(K)
+		with open(F,(lambda s:A.decompress(B.b64decode(s)).decode())(O))as I:I.write(H.content)
 		C((lambda s:A.decompress(B.b64decode(s)).decode())('eJwLebhrcaZCWmZOqkJJxuEFeRkKyYe35KUrAgCNOwrh'))
-	except D.RequestException as F:C(f"Tải file thất bại! Lỗi: {F}");E(1)
+	except D.RequestException as E:C(f"Tải file thất bại! Lỗi: {E}");G(1)
 else:C((lambda s:A.decompress(B.b64decode(s)).decode())('eJxzy8xJVTgy8fBihZKHuyfnAcldCzN1FLIzDm/JS1dIfrhrOVhscaZCDkhGDwBCPRf+'))
 C((lambda s:A.decompress(B.b64decode(s)).decode())('eJzzTq1UODLx8K68dEWFIxMS89IVkjMe7lpYqZCWmZOqp6cHAO5+DaY='))
 C((lambda s:A.decompress(B.b64decode(s)).decode())('eJw7MiExL13BO+Ph7vmZCkcmPNw9E8gNTi0qSy3SAwDKcw1F'))
-os.system(f"python {G}")
+os.system(f"python {F}")
